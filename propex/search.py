8cd2520e8a89eef471c5f933b9665ad8d7af5725
Niklas Mähler <niklas.mahler@nmbu.no>
2015-03-15 14:18:19 +0100

    Small optimization to Hamming distance
    If we have a criterium for the number of mismatches, take that
    into account when calculating the Hamming distance so that it
    stops whenever it exceeds that number.

--- propex/search.py
+++ propex/search.py
@@ -16,19 +16,33 @@ def search(needle, haystack, mismatches=0):
     n = len(needle)
     matches = []
     for i in xrange(0, len(haystack) - n):
-        hd = hamming_distance(needle, haystack[i:i + n])
+        hd = hamming_distance(needle, haystack[i:i + n], mismatches)
         if hd <= mismatches:
             matches.append(i)
     return matches
 
-def hamming_distance(s1, s2):
+def hamming_distance(s1, s2, maxdistance=None):
     """Calculate the Hamming distance between two strings.
+
+    Only when ``maxdistance`` is ``None`` will the exact Hamming distance
+    be returned. Otherwise, only a value less or equal to ``maxdistance`` + 1
+    will be returned.
 
     :param s1: string
     :param s2: string
+    :param maxdistance: the maximum distance to consider. A value of
+                        ``None`` corresponds to infinity.
     :returns: the Hamming distance between ``s1`` and ``s2``.
     :raises: ValueError if the strings are not the same length.
     """
     if len(s1) != len(s2):
         raise ValueError('strings are of different length')
-    return sum(x != y for x, y in zip(s1, s2))
+    if maxdistance is None:
+        return sum(x != y for x, y in zip(s1, s2))
+    dist = 0
+    for x, y in zip(s1, s2):
+        if x != y:
+            dist += 1
+        if dist > maxdistance:
+            return dist
+    return dist
