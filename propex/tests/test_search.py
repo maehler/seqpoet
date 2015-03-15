158b8e324b7c1037e649d0b2d56e801b63cdda16
Niklas Mähler <niklas.mahler@nmbu.no>
2015-03-15 17:25:59 +0100

    Add search test for genbank files

--- propex/tests/test_search.py
+++ propex/tests/test_search.py
@@ -1,8 +1,13 @@
-from nose.tools import raises
+from collections import defaultdict
 import os
+
+from nose.tools import raises
+from nose.plugins.skip import SkipTest
 
 from propex.search import search, hamming_distance
 from propex import Sequence
+from propex import GenBank
+from propex.genbank import Location
 
 class TestHammingDistance:
 
@@ -28,6 +33,9 @@ class TestSearch:
     def setUp(self):
         self.haystack = 'accgtgacgggcacgaggcatcattatctagcagcacatg'
         self.needle = 'gaggcat'
+        self.genbankdir = os.path.join(os.path.expanduser('~'), 'Dropbox',
+                                       'operon_extractor', 'data')
+        self.lmg718 = os.path.join(self.genbankdir, 'LMG718-cremoris.gb')
 
     def test_exact_match(self):
         res = search(self.needle, self.haystack)
@@ -41,3 +49,21 @@ class TestSearch:
 
         res = search('ggg', self.haystack, mismatches=1)
         assert res == [3, 7, 8, 9, 14, 15, 16], 'found {0}'.format(str(res))
+
+    def test_search_genbank(self):
+        if not os.path.exists(self.genbankdir):
+            raise SkipTest
+
+        gb = GenBank(self.lmg718)
+
+        with open(os.path.join(self.genbankdir, 'primers.txt')) as f:
+            probe = Sequence(f.readline().strip())
+
+        matches = defaultdict(list)
+        for locus in gb:
+            matches[locus.name].extend(search(str(probe), str(locus.seq), mismatches=0))
+
+        for locus, starts in matches.iteritems():
+            for s in starts:
+                for gbl in gb.get_locus_from_name(locus):
+                    assert gbl.seq[s:s + len(probe)] == probe
