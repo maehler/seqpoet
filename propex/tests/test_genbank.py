7e545488ac38cc958445d065a2f24d66a1471627
Niklas Mähler <niklas.mahler@gmail.com>
2015-04-19 15:36:44 +0200

    Test three line location string

--- propex/tests/test_genbank.py
+++ propex/tests/test_genbank.py
@@ -230,6 +230,18 @@ class TestGenBankFeature:
         assert gbf.get_qualifier('gene') == 'insZ'
         assert gbf.get_qualifier('locus_tag') == 'b4573'
 
+        feature = '''     CDS             complement(join(1294426..1294992,
+                     1294992..1295141,
+                     1295140..1295322))
+                     /gene="insZ"
+                     /locus_tag="b4573"'''
+
+        assert gbf.feature_type == 'CDS'
+        assert isinstance(gbf.location, JoinLocation)
+        assert len(gbf.location.locations) == 3
+        assert gbf.get_qualifier('gene') == 'insZ'
+        assert gbf.get_qualifier('locus_tag') == 'b4573'
+
     def test_empty_qualifiers(self):
         feature = '''     CDS             complement(52625..53704)
                      /gene="recF"
